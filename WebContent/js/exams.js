08f54ade57e188e6087e06c9f4b85a659474a479
Abhishek Srikanth <abhisrik99@outlook.com>
2020-12-06 14:36:41 +0530

    Make Quick Glance Dynamic

--- WebContent/js/exams.js
+++ WebContent/js/exams.js
@@ -1,4 +1,5 @@
 function showExam(n,cur) {
+	hideQuickGlance();
 	if(document.getElementById("Exam-"+cur).style.display == "block")
 	{
 		document.getElementById("Exam-"+cur).style.display = "none";
@@ -10,7 +11,7 @@ function showExam(n,cur) {
 	document.getElementById("Exam-"+cur).style.display = "block";
 }
 
-function showQuickGlance() {
+function toggleQuickGlance() {
 	if(document.getElementById("QuickGlanceTable").style.display == "block")
 	{
 		document.getElementById("QuickGlanceTable").style.display = "none";
@@ -21,12 +22,16 @@ function showQuickGlance() {
 	}
 }
 
+function hideQuickGlance() {
+	document.getElementById("QuickGlanceTable").style.display = "none";
+}
+
 function searchExam() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('a');
+    examsDiv = document.getElementById("myExams");
+    li = examsDiv.getElementsByTagName('a');
   
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
